77697f1203e20a42875d9148613ad60111791c4d
Tomoya <flyingfox193807@gmail.com>
2024-09-23 12:22:34 -0700

    finished ex07.js(3)

--- ex07.js
+++ ex07.js
@@ -1,6 +1,13 @@
 // Exercise 7: Write a function named 'calculateAverage' that takes an array of numbers and returns the average value.
 
 function calculateAverage(arr) {
+  let result = 0
+  let total= 0
+  for (i = 0; i < arr.length; i++) {
+    result += arr[i]
+  }
+  total = result/arr.length
+  return total
   // your code here
 }
 
