edac5ab0e292550521951f8fe5abdc67754ffa18
Tomoya <flyingfox193807@gmail.com>
2024-09-24 10:57:59 -0700

    finished ex13.js(7)

--- ex13.js
+++ ex13.js
@@ -11,32 +11,6 @@ function findById(arr, id) {
   // your code here
 }
 
-console.log(findById([{ ID: 1, name: 'John' }, { ID: 2, name: 'Jane' }, {ID: 3, name: `Ethan`}], 2)) // { id: 2, name: 'Jane' }
+console.log(findById([{ ID: 1, name: 'John' }, { ID: 2, name: 'Jane' }, {ID: 3, name: `Ethan`}, {ID: 4, name: `Kaylee`}], 2)) // { id: 2, name: 'Jane' }
 
-// const student = {
-//   name: "Daniel",
-//   college: "Cornerstone",
-//   age: 24,
-//   rollNumber: 1000,
-// }
-
-// for (let stud in student) {
-//   console.log(student[stud])
-// }
-
-// console.log(student.college)
-
-// var donuts = [
-//   { type: "Jelly", cost: 1.22 },
-//   { type: "Chocolate", cost: 2.45 },
-//   { type: "Cider", cost: 1.59 },
-//   { type: "Boston Cream", cost: 5.99 }
-// ];
-
-// for (let donu in donuts) {
-//   console.log(`${donuts[donu].type} donuts cost $${donuts[donu].cost} each.`)
-//   console.log(donuts[donu].type)
-// }
-
-// console.log(donuts[1].type)
 
