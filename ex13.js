edef230997a2cb44190fc08454cd0403952d1de1
Tomoya <flyingfox193807@gmail.com>
2024-09-24 10:40:54 -0700

    finished ex18.js(6)

--- ex13.js
+++ ex13.js
@@ -1,7 +1,42 @@
 // Exercise 13: Write a function named 'findById' that takes an array of objects and an id, and returns the object with the matching id, or null if not found.
 
 function findById(arr, id) {
+  let result
+  for (i = 0; i < arr.length; i++) {
+    if (arr[i].ID === id) {
+      result = arr[i]
+    }
+  }
+  return result
   // your code here
 }
 
-console.log(findById([{ id: 1, name: 'John' }, { id: 2, name: 'Jane' }], 2)) // { id: 2, name: 'Jane' }
+console.log(findById([{ ID: 1, name: 'John' }, { ID: 2, name: 'Jane' }, {ID: 3, name: `Ethan`}], 2)) // { id: 2, name: 'Jane' }
+
+// const student = {
+//   name: "Daniel",
+//   college: "Cornerstone",
+//   age: 24,
+//   rollNumber: 1000,
+// }
+
+// for (let stud in student) {
+//   console.log(student[stud])
+// }
+
+// console.log(student.college)
+
+// var donuts = [
+//   { type: "Jelly", cost: 1.22 },
+//   { type: "Chocolate", cost: 2.45 },
+//   { type: "Cider", cost: 1.59 },
+//   { type: "Boston Cream", cost: 5.99 }
+// ];
+
+// for (let donu in donuts) {
+//   console.log(`${donuts[donu].type} donuts cost $${donuts[donu].cost} each.`)
+//   console.log(donuts[donu].type)
+// }
+
+// console.log(donuts[1].type)
+
