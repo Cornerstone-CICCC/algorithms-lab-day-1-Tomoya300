de58a39182890161a77c0c2654c01fa6096860b8
Tomoya <flyingfox193807@gmail.com>
2024-09-25 10:23:08 -0700

    finished ex16.js

--- ex16.js
+++ ex16.js
@@ -1,7 +1,16 @@
 // Exercise 16: Write a function named 'sumNestedArray' that takes an array of arrays of numbers and returns the sum of all numbers.
 
 function sumNestedArray(arr) {
-  // your code here
+  let result = 0
+  for (i = 0; i < arr.length; i++) {
+    for (k = 0; k < arr[i].length; k++) {
+      result += arr[i][k]
+    }
+  }
+  return result
 }
 
 console.log(sumNestedArray([[1, 2, 3], [4, 5], [6, 7, 8]])) // 36
+
+// let array = [[1, 2, 3], [4, 5], [5, 6, 6, 7]]
+// console.log(array[0].length)
